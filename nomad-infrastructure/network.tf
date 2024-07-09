a3f48ac73a95ed80a10fc2b5dea7ce611c575a47
devops-rob <robert.raybarnes@gmail.com>
2024-07-09 20:15:05 +0100

    removing healthcheck

--- nomad-infrastructure/network.tf
+++ nomad-infrastructure/network.tf
@@ -32,41 +32,41 @@ resource "aws_lb" "nomad" {
   }
 }
 
-#resource "aws_lb_target_group" "nomad" {
-#  name        = "nomad-tg"
-#  port        = 4646
-#  protocol    = "HTTP"
-#  vpc_id      = module.vpc.vpc_id
-#  target_type = "instance"
-#
-##  health_check {
-##    path                = "/v1/status/leader"
-##    interval            = 30
-##    timeout             = 5
-##    healthy_threshold   = 2
-##    unhealthy_threshold = 2
-##    matcher             = "200"
-##  }
-#
-#  tags = {
-#    Name = "nomad-tg"
+resource "aws_lb_target_group" "nomad" {
+  name        = "nomad-tg"
+  port        = 4646
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
+  target_type = "instance"
+
+#  health_check {
+#    path                = "/v1/status/leader"
+#    interval            = 30
+#    timeout             = 5
+#    healthy_threshold   = 2
+#    unhealthy_threshold = 2
+#    matcher             = "200"
 #  }
-#}
-#
-#resource "aws_lb_listener" "nomad" {
-#  load_balancer_arn = aws_lb.nomad.arn
-#  port              = 80
-#  protocol          = "HTTP"
-#
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.nomad.arn
-#  }
-#}
-#
-#resource "aws_lb_target_group_attachment" "nomad" {
-#  count            = var.server_count
-#  target_group_arn = aws_lb_target_group.nomad.arn
-#  target_id        = aws_instance.nomad_servers[count.index].id
-#  port             = 4646
-#}
+
+  tags = {
+    Name = "nomad-tg"
+  }
+}
+
+resource "aws_lb_listener" "nomad" {
+  load_balancer_arn = aws_lb.nomad.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nomad.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "nomad" {
+  count            = var.server_count
+  target_group_arn = aws_lb_target_group.nomad.arn
+  target_id        = aws_instance.nomad_servers[count.index].id
+  port             = 4646
+}
