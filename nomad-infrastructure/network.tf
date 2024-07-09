aa18ef22e3c5e9e22563866910ed9a53c4733417
devops-rob <robert.raybarnes@gmail.com>
2024-07-09 19:36:15 +0100

    adding lb back in

--- nomad-infrastructure/network.tf
+++ nomad-infrastructure/network.tf
@@ -18,55 +18,55 @@ module "vpc" {
   }
 }
 
-#resource "aws_lb" "nomad" {
-#  name               = "nomad-lb"
-#  internal           = false
-#  load_balancer_type = "application"
-#  security_groups    = [aws_security_group.nomad.id]
-#  subnets            = [element(module.vpc.private_subnets, 0), element(module.vpc.private_subnets, 1), element(module.vpc.private_subnets, 2)]
-#
-#  enable_deletion_protection = false
-#
-#  tags = {
-#    Name = "nomad-lb"
-#  }
-#}
-#
-#resource "aws_lb_target_group" "nomad" {
-#  name        = "nomad-tg"
-#  port        = 4646
-#  protocol    = "HTTP"
-#  vpc_id      = module.vpc.vpc_id
-#  target_type = "instance"
-#
-#  health_check {
-#    path                = "/v1/status/leader"
-#    interval            = 30
-#    timeout             = 5
-#    healthy_threshold   = 2
-#    unhealthy_threshold = 2
-#    matcher             = "200"
-#  }
-#
-#  tags = {
-#    Name = "nomad-tg"
-#  }
-#}
-#
-#resource "aws_lb_listener" "nomad" {
-#  load_balancer_arn = aws_lb.nomad.arn
-#  port              = 80
-#  protocol          = "HTTP"
-#
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.nomad.arn
-#  }
-#}
-#
-#resource "aws_lb_target_group_attachment" "nomad" {
-#  count            = var.server_count
-#  target_group_arn = aws_lb_target_group.nomad.arn
-#  target_id        = aws_instance.nomad_servers[count.index].id
-#  port             = 4646
-#}
+resource "aws_lb" "nomad" {
+  name               = "nomad-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.nomad.id]
+  subnets            = [element(module.vpc.private_subnets, 0), element(module.vpc.private_subnets, 1), element(module.vpc.private_subnets, 2)]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "nomad-lb"
+  }
+}
+
+resource "aws_lb_target_group" "nomad" {
+  name        = "nomad-tg"
+  port        = 4646
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
+  target_type = "instance"
+
+  health_check {
+    path                = "/v1/status/leader"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "nomad-tg"
+  }
+}
+
+resource "aws_lb_listener" "nomad" {
+  load_balancer_arn = aws_lb.nomad.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nomad.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "nomad" {
+  count            = var.server_count
+  target_group_arn = aws_lb_target_group.nomad.arn
+  target_id        = aws_instance.nomad_servers[count.index].id
+  port             = 4646
+}
