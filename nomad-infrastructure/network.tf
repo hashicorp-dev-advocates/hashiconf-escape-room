ccd28b8d7b5db4f4433acf25e7d3c3a820bf5f13
devops-rob <robert.raybarnes@gmail.com>
2024-07-09 19:47:38 +0100

    adding additional az

--- nomad-infrastructure/network.tf
+++ nomad-infrastructure/network.tf
@@ -5,7 +5,7 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = var.availability_zones
-  private_subnets = ["10.0.1.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 
   enable_nat_gateway   = true
