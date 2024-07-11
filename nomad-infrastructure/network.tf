c2ef1e552bfbe9768c557e0b88063c662e003b23
devops-rob <robert.raybarnes@gmail.com>
2024-07-11 13:34:58 +0100

    lb config update

--- nomad-infrastructure/network.tf
+++ nomad-infrastructure/network.tf
@@ -6,7 +6,7 @@ module "vpc" {
 
   azs             = var.availability_zones
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway   = true
   enable_vpn_gateway   = false
