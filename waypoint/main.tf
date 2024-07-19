0874fdc6471097fbcca79a465ce0e00cd37a5263
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-07-19 11:56:14 -0400

    Add back Waypoint template

--- waypoint/main.tf
+++ waypoint/main.tf
@@ -35,55 +35,55 @@ resource "tfe_team_project_access" "waypoint" {
 
 data "nomad_node_pools" "all" {}
 
-# resource "hcp_waypoint_template" "nomad_app" {
-#   name    = "nomad-container-application"
-#   summary = "Deploy a container application to Nomad"
+resource "hcp_waypoint_template" "nomad_app" {
+  name    = "nomad-container-application"
+  summary = "Deploy a container application to Nomad"
 
-#   terraform_cloud_workspace_details = {
-#     name                 = "nomad-app"
-#     terraform_project_id = tfe_project.waypoint.id
-#   }
+  terraform_cloud_workspace_details = {
+    name                 = tfe_project.waypoint.name
+    terraform_project_id = tfe_project.waypoint.id
+  }
 
-#   terraform_no_code_module = {
-#     source  = "app.terraform.io/hashicorp-team-da-beta/app/nomad"
-#     version = "0.0.0"
-#   }
+  terraform_no_code_module = {
+    source  = "app.terraform.io/hashicorp-team-da-beta/app/nomad"
+    version = "0.0.0"
+  }
 
-#   variable_options = [
-#     {
-#       name          = "application_name"
-#       variable_type = "string"
-#     },
-#     {
-#       name          = "application_port"
-#       variable_type = "number"
-#     },
-#     {
-#       name          = "image"
-#       variable_type = "string"
-#     },
-#     {
-#       name          = "metadata"
-#       variable_type = "map(string)"
-#     },
-#     {
-#       name          = "node_pool"
-#       variable_type = "string"
-#       options       = [for pool in data.nomad_node_pools.all.node_pools : pool.name]
-#       user_editable = false
-#     }
-#     ,
-#     {
-#       name          = "driver"
-#       variable_type = "string"
-#       options       = ["docker"]
-#       user_editable = false
-#     },
-#     {
-#       name          = "service_provider"
-#       variable_type = "string"
-#       options       = ["nomad"]
-#       user_editable = false
-#     }
-#   ]
-# }
+  variable_options = [
+    {
+      name          = "application_name"
+      variable_type = "string"
+    },
+    {
+      name          = "application_port"
+      variable_type = "number"
+    },
+    {
+      name          = "image"
+      variable_type = "string"
+    },
+    {
+      name          = "metadata"
+      variable_type = "map(string)"
+    },
+    {
+      name          = "node_pool"
+      variable_type = "string"
+      options       = [for pool in data.nomad_node_pools.all.node_pools : pool.name]
+      user_editable = false
+    }
+    ,
+    {
+      name          = "driver"
+      variable_type = "string"
+      options       = ["docker"]
+      user_editable = false
+    },
+    {
+      name          = "service_provider"
+      variable_type = "string"
+      options       = ["nomad"]
+      user_editable = false
+    }
+  ]
+}
