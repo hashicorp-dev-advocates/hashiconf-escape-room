eb7934c014a92408a6c9c259a91122003a084936
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-07-19 11:49:54 -0400

    Comment out Waypoint templates for now

--- waypoint/main.tf
+++ waypoint/main.tf
@@ -16,10 +16,10 @@ resource "tfe_team_token" "waypoint" {
   expired_at = time_rotating.waypoint_token.rotation_rfc3339
 }
 
-resource "hcp_waypoint_tfc_config" "org" {
-  token        = tfe_team_token.waypoint.token
-  tfc_org_name = tfe_team.waypoint.organization
-}
+# resource "hcp_waypoint_tfc_config" "org" {
+#   token        = tfe_team_token.waypoint.token
+#   tfc_org_name = tfe_team.waypoint.organization
+# }
 
 resource "tfe_project" "waypoint" {
   organization = data.tfe_organization.org.name
@@ -34,55 +34,55 @@ resource "tfe_team_project_access" "waypoint" {
 
 data "nomad_node_pools" "all" {}
 
-resource "hcp_waypoint_template" "nomad_app" {
-  name    = "nomad-container-application"
-  summary = "Deploy a container application to Nomad"
+# resource "hcp_waypoint_template" "nomad_app" {
+#   name    = "nomad-container-application"
+#   summary = "Deploy a container application to Nomad"
 
-  terraform_cloud_workspace_details = {
-    name                 = "nomad-app"
-    terraform_project_id = tfe_project.waypoint.id
-  }
+#   terraform_cloud_workspace_details = {
+#     name                 = "nomad-app"
+#     terraform_project_id = tfe_project.waypoint.id
+#   }
 
-  terraform_no_code_module = {
-    source  = "app.terraform.io/hashicorp-team-da-beta/app/nomad"
-    version = "0.0.0"
-  }
+#   terraform_no_code_module = {
+#     source  = "app.terraform.io/hashicorp-team-da-beta/app/nomad"
+#     version = "0.0.0"
+#   }
 
-  variable_options = [
-    {
-      name          = "application_name"
-      variable_type = "string"
-    },
-    {
-      name          = "application_port"
-      variable_type = "number"
-    },
-    {
-      name          = "image"
-      variable_type = "string"
-    },
-    {
-      name          = "metadata"
-      variable_type = "map(string)"
-    },
-    {
-      name          = "node_pool"
-      variable_type = "string"
-      options       = [for pool in data.nomad_node_pools.all.node_pools : pool.name]
-      user_editable = false
-    }
-    ,
-    {
-      name          = "driver"
-      variable_type = "string"
-      options       = ["docker"]
-      user_editable = false
-    },
-    {
-      name          = "service_provider"
-      variable_type = "string"
-      options       = ["nomad"]
-      user_editable = false
-    }
-  ]
-}
+#   variable_options = [
+#     {
+#       name          = "application_name"
+#       variable_type = "string"
+#     },
+#     {
+#       name          = "application_port"
+#       variable_type = "number"
+#     },
+#     {
+#       name          = "image"
+#       variable_type = "string"
+#     },
+#     {
+#       name          = "metadata"
+#       variable_type = "map(string)"
+#     },
+#     {
+#       name          = "node_pool"
+#       variable_type = "string"
+#       options       = [for pool in data.nomad_node_pools.all.node_pools : pool.name]
+#       user_editable = false
+#     }
+#     ,
+#     {
+#       name          = "driver"
+#       variable_type = "string"
+#       options       = ["docker"]
+#       user_editable = false
+#     },
+#     {
+#       name          = "service_provider"
+#       variable_type = "string"
+#       options       = ["nomad"]
+#       user_editable = false
+#     }
+#   ]
+# }
---
+++ waypoint/outputs.tf
@@ -0,0 +1,4 @@
+output "waypoint_team_token" {
+  value     = tfe_team_token.waypoint.token
+  sensitive = true
+}
