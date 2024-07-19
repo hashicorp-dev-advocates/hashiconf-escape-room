2ba01900a36574fd759f6622ccababc3a9652ff8
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-07-19 13:08:15 -0400

    Add back Terraform module

--- waypoint/tf-module.tf
+++ waypoint/tf-module.tf
@@ -7,21 +7,21 @@ data "tfe_oauth_client" "client" {
   name         = var.github_user
 }
 
-# resource "tfe_registry_module" "nomad_app" {
-#   organization = data.tfe_organization.org.name
+resource "tfe_registry_module" "nomad_app" {
+  organization = data.tfe_organization.org.name
 
-#   test_config {
-#     tests_enabled = false
-#   }
+  test_config {
+    tests_enabled = false
+  }
 
-#   vcs_repo {
-#     branch             = "main"
-#     display_identifier = "${var.github_user}/terraform-nomad-app"
-#     identifier         = "${var.github_user}/terraform-nomad-app"
-#     oauth_token_id     = data.tfe_oauth_client.client.oauth_token_id
-#     tags               = false
-#   }
-# }
+  vcs_repo {
+    branch             = "main"
+    display_identifier = "${var.github_user}/terraform-nomad-app"
+    identifier         = "${var.github_user}/terraform-nomad-app"
+    oauth_token_id     = data.tfe_oauth_client.client.oauth_token_id
+    tags               = false
+  }
+}
 
 # resource "tfe_no_code_module" "foobar" {
 #     organization = data.tfe_organization.org.id
