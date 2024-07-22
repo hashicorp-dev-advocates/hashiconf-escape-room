e329d75172f6cf8dc7ce65ea5c9a64970a45aff1
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-07-22 16:15:57 -0400

    Add template ID

--- waypoint/outputs.tf
+++ waypoint/outputs.tf
@@ -1,4 +1,4 @@
-output "waypoint_team_token" {
-  value     = tfe_team_token.waypoint.token
-  sensitive = true
+output "nomad_app_template_id" {
+  value       = hcp_waypoint_template.nomad_app.id
+  description = "Waypoint template ID for Nomad application"
 }
