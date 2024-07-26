e835b64637a6100f3fc56d31e460f70eb9e4d992
devops-rob <robert.raybarnes@gmail.com>
2024-07-26 11:38:32 +0100

    vault token

--- vault-config/providers.tf
+++ vault-config/providers.tf
@@ -14,3 +14,12 @@ terraform {
 provider "hcp" {
   project_id = var.hcp_project_id
 }
+
+resource "hcp_vault_cluster_admin_token" "vault" {
+  cluster_id = data.terraform_remote_state.hcp.outputs.vault.cluster_id
+}
+
+provider "vault" {
+  address = data.terraform_remote_state.hcp.outputs.vault.public_endpoint
+  token = hcp_vault_cluster_admin_token.vault.token
+}
