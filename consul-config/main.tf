6eca058b8f6252da10419e6e5cb1432bfab9b9e6
devops-rob <robert.raybarnes@gmail.com>
2024-07-30 17:09:46 +0100

    default services and nodes

--- consul-config/main.tf
+++ consul-config/main.tf
@@ -1,3 +1,27 @@
 resource "hcp_consul_cluster_root_token" "root" {
   cluster_id = data.terraform_remote_state.hcp.outputs.consul.cluster_id
 }
+
+resource "consul_node" "nodes" {
+  for_each = {
+    for svc in var.services :
+    svc.service_name => svc
+  }
+
+  address = each.value["node_address"]
+  name    = each.value["node_name"]
+}
+
+resource "consul_service" "services" {
+
+  for_each = {
+    for svc in var.services :
+        svc.service_name => svc
+  }
+
+  name = each.value["service_name"]
+  node = each.value["node_name"]
+  meta =each.value["meta"]
+  port = each.value["port"]
+  tags = each.value["tags"]
+}
--- consul-config/variables.tf
+++ consul-config/variables.tf
@@ -1,3 +1,30 @@
 variable "hcp_project_id" {
   description = "HCP Project ID"
 }
+
+variable "services" {
+  type = list(
+    object(
+      {
+        service_name = string
+        node_address = string
+        node_name    = string
+        port         = number
+        meta         = map(string)
+        tags         = list(string)
+      }
+    )
+  )
+  default = [
+    {
+      service_name = "google"
+      node_address = "www.google.com"
+      node_name    = "google"
+      port         = 80
+      meta = {
+        default = "true"
+      }
+      tags = null
+    }
+  ]
+}
