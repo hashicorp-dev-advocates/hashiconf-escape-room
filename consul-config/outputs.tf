3228832f333f28d983aaf834499f79e86616e8b2
devops-rob <robert.raybarnes@gmail.com>
2024-07-30 17:14:54 +0100

    outputs for testing

---
+++ consul-config/outputs.tf
@@ -0,0 +1,8 @@
+output "consul_token" {
+  value     = hcp_consul_cluster_root_token.root.secret_id
+  sensitive = true
+}
+
+output "consul_addr" {
+  value = data.terraform_remote_state.hcp.outputs.consul.public_endpoint
+}
