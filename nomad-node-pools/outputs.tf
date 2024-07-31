ee23b804218ccfaef76f36c52e159bb5e0e3656e
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2024-07-31 15:16:33 -0400

    Add outputs for HCP packer artifacts

--- nomad-node-pools/outputs.tf
+++ nomad-node-pools/outputs.tf
@@ -1,6 +1,4 @@
 output "hcp_packer_artifacts" {
-  value = {
-    bucket_names         = data.hcp_packer_artifact.packer.*.bucket_name
-    external_identifiers = data.hcp_packer_artifact.packer.*.external_identifier
-  }
+  value       = { for a in data.hcp_packer_artifact.packer : a.bucket_name => a.external_identifier... }
+  description = "AMI IDs used with HCP Packer buckets for images"
 }
