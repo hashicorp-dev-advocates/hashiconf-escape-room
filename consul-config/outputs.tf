2615bf45b9c58fcac8155a241760fbcf58c71a3f
devops-rob <robert.raybarnes@gmail.com>
2024-08-01 12:10:03 +0100

    config file output

--- consul-config/outputs.tf
+++ consul-config/outputs.tf
@@ -6,3 +6,7 @@ output "consul_token" {
 output "consul_addr" {
   value = data.terraform_remote_state.hcp.outputs.consul.public_endpoint
 }
+
+output "hcp_file" {
+  value = data.terraform_remote_state.hcp.outputs.config_file
+}
