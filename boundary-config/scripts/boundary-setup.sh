1ec3bcb7a2f7710f2787931557dc585814eeb2a4
devops-rob <robert.raybarnes@gmail.com>
2024-08-05 13:30:24 +0100

    updating persistent auth path

--- boundary-config/scripts/boundary-setup.sh
+++ boundary-config/scripts/boundary-setup.sh
@@ -5,6 +5,7 @@ wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/sha
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install boundary-enterprise
 
+mkdir "/boundary/auth_data"
 # Write the config
 cat <<EOT > /etc/boundary.d/boundary.hcl
   disable_mlock = true
@@ -18,7 +19,7 @@ cat <<EOT > /etc/boundary.d/boundary.hcl
   }
 
   worker {
-    auth_storage_path="/etc/boundary.d/auth_data"
+    auth_storage_path="/boundary/auth_data"
 
     controller_generated_activation_token = "${CONTROLLER_GENERATED_ACTIVATION_TOKEN}"
 
