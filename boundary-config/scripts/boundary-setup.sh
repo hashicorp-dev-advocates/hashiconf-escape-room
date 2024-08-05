4bfdc9ac9eabc8c743e6c8ee04b388c8a1c9e01c
devops-rob <robert.raybarnes@gmail.com>
2024-08-05 14:40:36 +0100

    updating persistent auth path directory permissions

--- boundary-config/scripts/boundary-setup.sh
+++ boundary-config/scripts/boundary-setup.sh
@@ -5,7 +5,8 @@ wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/sha
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install boundary-enterprise
 
-mkdir "/boundary/auth_data"
+sudo mkdir -p "/boundary/auth_data"
+sudo chmod -R 777 /boundary/auth_data
 # Write the config
 cat <<EOT > /etc/boundary.d/boundary.hcl
   disable_mlock = true
