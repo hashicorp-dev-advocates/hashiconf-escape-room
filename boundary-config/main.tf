6886ea4e77fb55b2defbdd99266d116469112fbf
devops-rob <robert.raybarnes@gmail.com>
2024-08-06 17:14:20 +0100

    readding worker

--- boundary-config/main.tf
+++ boundary-config/main.tf
@@ -1,8 +1,8 @@
-#resource "boundary_worker" "main" {
-#  scope_id    = "global"
-#  name        = "main"
-#  description = "main public subnet worker"
-#}
+resource "boundary_worker" "main" {
+  scope_id    = "global"
+  name        = "main"
+  description = "main private subnet worker"
+}
 
 resource "aws_security_group" "boundary" {
   vpc_id = data.terraform_remote_state.nomad.outputs.vpc_id
@@ -46,32 +46,32 @@ locals {
   )
 }
 
-#resource "aws_instance" "boundary_worker_public" {
-#
-#  ami                         = data.aws_ami.ubuntu.id
-#  instance_type               = "t3.micro"
-#  subnet_id                   = data.aws_subnet.private.id
-#  key_name                    = data.aws_key_pair.deployer.key_name
-#  associate_public_ip_address = false
-#
-#  user_data = templatefile("./scripts/boundary-setup.sh", {
-#    CLUSTER_ID                            = data.terraform_remote_state.hcp.outputs.boundary.cluster_id
-#    CONTROLLER_GENERATED_ACTIVATION_TOKEN = boundary_worker.main.controller_generated_activation_token
-#  })
-#
-#  vpc_security_group_ids = local.combined_security_group_ids
-#
-#  tags = {
-#    Name = "Boundary Worker Public"
-#  }
-#
-#  lifecycle {
-#    ignore_changes = [
-#      user_data,
-#      ami
-#    ]
-#  }
-#}
+resource "aws_instance" "boundary_worker_public" {
+
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  subnet_id                   = data.aws_subnet.private.id
+  key_name                    = data.aws_key_pair.deployer.key_name
+  associate_public_ip_address = false
+
+  user_data = templatefile("./scripts/boundary-setup.sh", {
+    CLUSTER_ID                            = data.terraform_remote_state.hcp.outputs.boundary.cluster_id
+    CONTROLLER_GENERATED_ACTIVATION_TOKEN = boundary_worker.main.controller_generated_activation_token
+  })
+
+  vpc_security_group_ids = local.combined_security_group_ids
+
+  tags = {
+    Name = "Boundary Worker Private"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      user_data,
+      ami
+    ]
+  }
+}
 
 resource "boundary_scope" "hashiconf_escape_room_org" {
   scope_id                 = "global"
