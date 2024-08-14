fa042365e534790ce94700c7989db6765b76311b
devops-rob <robert.raybarnes@gmail.com>
2024-08-14 15:31:15 +0100

    removing old consul clients without boundary clues

--- consul-config/variables.tf
+++ consul-config/variables.tf
@@ -10,9 +10,10 @@ variable "services" {
   type = list(
     object(
       {
-        service_name = string
-        meta         = map(string)
-        tags         = list(string)
+        service_name  = string
+        meta          = map(string)
+        tags          = list(string)
+        boundary_clue = string
       }
     )
   )
@@ -22,35 +23,41 @@ variable "services" {
       meta = {
         clue = "Look at the tag of the Consul shirt."
       }
-      tags = null
+      tags          = null
+      boundary_clue = "Look at the tag of the Boundary shirt."
     },
     {
       service_name = "wishlist"
       meta = {
         clue = "Look under the mug on the shelf."
       }
-      tags = null
+      tags          = null
+      boundary_clue = "Look under the mug next to the laptop."
     },
     {
       service_name = "recommendation"
       meta = {
         clue = "Look at the handle of the umbrella."
       }
-      tags = null
+      tags          = null
+      boundary_clue = "Look at the tag of the pride shirt."
     },
     {
       service_name = "notification"
       meta = {
         clue = "Look at the top of the garment rack."
       }
-      tags = null
+      tags          = null
+      boundary_clue = "Look at the photo of HashiConf 2019 in the photo album."
     },
     {
       service_name = "warehouse"
       meta = {
         clue = "Look on the inside cover of the book, “Consul: Up and Running”."
       }
-      tags = null
+      tags          = null
+      boundary_clue = "Look at the tag of the Waypoint shirt."
+
     }
   ]
 }
