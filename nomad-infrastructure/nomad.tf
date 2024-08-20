efaf4017f7e92e69d3d380a5b492d99d5ccd9132
devops-rob <robert.raybarnes@gmail.com>
2024-08-20 16:55:02 +0100

    removing nomad

--- nomad-infrastructure/nomad.tf
+++ nomad-infrastructure/nomad.tf
@@ -1,31 +1,31 @@
-#resource "terracurl_request" "nomad_status" {
-#  method         = "GET"
-#  name           = "nomad_status"
-#  response_codes = [200]
-#  url            = "http://${aws_lb.nomad.dns_name}/v1/status/leader"
-#  max_retry      = 4
-#  retry_interval = 10
-#
-#  depends_on = [
-#    aws_instance.nomad_servers,
-#    aws_lb.nomad,
-#    aws_lb_listener.web,
-#    aws_lb_target_group.nomad,
-#    aws_lb_target_group_attachment.nomad
-#  ]
-#}
-#
-#resource "terracurl_request" "bootstrap_acl" {
-#  method         = "POST"
-#  name           = "bootstrap"
-#  response_codes = [200, 201]
-#  url            = "http://${aws_lb.nomad.dns_name}/v1/acl/bootstrap"
-#
-#  lifecycle {
-#    ignore_changes = all
-#  }
-#
-#  depends_on = [
-#    terracurl_request.nomad_status,
-#  ]
-#}
+resource "terracurl_request" "nomad_status" {
+  method         = "GET"
+  name           = "nomad_status"
+  response_codes = [200]
+  url            = "http://${aws_lb.nomad.dns_name}/v1/status/leader"
+  max_retry      = 4
+  retry_interval = 10
+
+  depends_on = [
+    aws_instance.nomad_servers,
+    aws_lb.nomad,
+    aws_lb_listener.web,
+    aws_lb_target_group.nomad,
+    aws_lb_target_group_attachment.nomad
+  ]
+}
+
+resource "terracurl_request" "bootstrap_acl" {
+  method         = "POST"
+  name           = "bootstrap"
+  response_codes = [200, 201]
+  url            = "http://${aws_lb.nomad.dns_name}/v1/acl/bootstrap"
+
+  lifecycle {
+    ignore_changes = all
+  }
+
+  depends_on = [
+    terracurl_request.nomad_status,
+  ]
+}
