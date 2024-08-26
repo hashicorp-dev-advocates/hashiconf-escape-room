03b704378d7efb146e6b558b212f9f28daf773a5
devops-rob <robert.raybarnes@gmail.com>
2024-08-26 15:07:23 -0700

    storage bucket

--- boundary-config/session-recording.tf
+++ boundary-config/session-recording.tf
@@ -11,25 +11,25 @@ resource "aws_s3_access_point" "backend_recordings" {
   }
 }
 
-#resource "boundary_storage_bucket" "backend_recordings" {
-#  name        = "hashiconf_escape_room"
-#  description = "All the recordings for HashiConf Escape Room"
-#  scope_id    = boundary_scope.hashiconf_escape_room_org.id
-#  plugin_name = "aws"
-#  bucket_name = aws_s3_bucket.hashiconf.bucket
-#  attributes_json = jsonencode({
-#    "region"                      = var.aws_region
-#    "disable_credential_rotation" = true
-#    "role_arn" = aws_iam_role.boundary_worker.name
-#  })
-#
-#  worker_filter = <<EOF
-#"/name" == "session-recording"
-#EOF
-#
-#
-#  depends_on = [
-#    aws_s3_access_point.backend_recordings,
-#    aws_instance.boundary_worker_session_recording
-#  ]
-#}
+resource "boundary_storage_bucket" "backend_recordings" {
+  name        = "hashiconf_escape_room"
+  description = "All the recordings for HashiConf Escape Room"
+  scope_id    = boundary_scope.hashiconf_escape_room_org.id
+  plugin_name = "aws"
+  bucket_name = aws_s3_bucket.hashiconf.bucket
+  attributes_json = jsonencode({
+    "region"                      = var.aws_region
+    "disable_credential_rotation" = true
+    "role_arn" = aws_iam_role.boundary_worker.name
+  })
+
+  worker_filter = <<EOF
+"/name" == "session-recording"
+EOF
+
+
+  depends_on = [
+    aws_s3_access_point.backend_recordings,
+    aws_instance.boundary_worker_session_recording
+  ]
+}
