589968d17708fd4eee4cd5be34d68f8aa217a4ad
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2025-05-16 12:54:32 -0400

    Add client secret to GitHub actions

--- hcp-infrastructure/github.tf
+++ hcp-infrastructure/github.tf
@@ -8,22 +8,20 @@ resource "hcp_project_iam_binding" "github_actions" {
   role         = "roles/contributor"
 }
 
-resource "hcp_iam_workload_identity_provider" "github_actions" {
-  name              = "github-actions"
+resource "hcp_service_principal_key" "github_actions" {
   service_principal = hcp_service_principal.github_actions.resource_name
-  description       = "Allow HCP Packer deploy workflow to access github actions service principal"
-
-  oidc = {
-    issuer_uri = "https://token.actions.githubusercontent.com"
-  }
-
-  conditional_access = "jwt_claims.repository == `${var.github_user}/${var.repositories.0}` and jwt_claims.ref == `refs/heads/main`"
 }
 
-resource "github_actions_variable" "github_actions_provider_name" {
-  repository    = var.repositories.0
-  variable_name = "PROVIDER_NAME"
-  value         = "iam/project/${var.hcp_project_id}/service-principal/${hcp_service_principal.github_actions.name}/workload-identity-provider/${hcp_iam_workload_identity_provider.github_actions.name}"
+resource "github_actions_secret" "hcp_service_principal_client_id" {
+  repository      = var.repositories.0
+  secret_name     = "HCP_CLIENT_ID"
+  plaintext_value = hcp_service_principal_key.github_actions.client_id
+}
+
+resource "github_actions_secret" "hcp_service_principal_client_secret" {
+  repository      = var.repositories.0
+  secret_name     = "HCP_CLIENT_SECRET"
+  plaintext_value = hcp_service_principal_key.github_actions.client_secret
 }
 
 resource "github_actions_variable" "aws_region" {
