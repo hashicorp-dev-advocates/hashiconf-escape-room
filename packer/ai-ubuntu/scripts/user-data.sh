8f20188e0fdd0a618f978fc0e0f04419ff88dbf4
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2025-05-29 16:01:09 -0400

    Move EBS volume mount into user data script

--- packer/ai-ubuntu/scripts/user-data.sh
+++ packer/ai-ubuntu/scripts/user-data.sh
@@ -4,6 +4,17 @@ TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-meta
 NODE_POOL=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/tags/instance/NodePool)
 AWS_REGION=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/region)
 
+# Add EBS volume for Docker
+mkfs -t xfs /dev/nvme1n1
+mkdir -p /var/lib/docker
+mount /dev/nvme1n1 /var/lib/docker
+
+chgrp docker /var/lib/docker
+chmod g+rwx /var/lib/docker
+
+systemctl enable docker
+systemctl restart docker
+
 # Nomad configuration files
 cat <<EOF > /etc/nomad.d/nomad.hcl
 log_level = "INFO"
