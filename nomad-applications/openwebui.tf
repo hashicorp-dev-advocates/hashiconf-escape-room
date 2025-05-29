0061bcd284dd1682bab7549132f8d81d8aa5fede
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2025-05-29 11:55:57 -0400

    Redeploy Open WebUI

--- nomad-applications/openwebui.tf
+++ nomad-applications/openwebui.tf
@@ -1,78 +1,78 @@
-# resource "nomad_dynamic_host_volume" "open_webui" {
-#   name      = "open-webui"
-#   namespace = "default"
+resource "nomad_dynamic_host_volume" "open_webui" {
+  name      = "open-webui"
+  namespace = "default"
 
-#   plugin_id = "mkdir"
+  plugin_id = "mkdir"
 
-#   capacity_max = "8.0 GiB"
-#   capacity_min = "1.0 GiB"
+  capacity_max = "8.0 GiB"
+  capacity_min = "1.0 GiB"
 
-#   capability {
-#     access_mode     = "single-node-writer"
-#     attachment_mode = "file-system"
-#   }
+  capability {
+    access_mode     = "single-node-writer"
+    attachment_mode = "file-system"
+  }
 
-#   node_pool = "gpu"
-# }
+  node_pool = "gpu"
+}
 
-# resource "nomad_job" "open_webui" {
-#   jobspec = <<EOT
-# job "openwebui" {
-#   datacenters = ["dc1"]
-#   type        = "service"
-#   node_pool   = "gpu"
+resource "nomad_job" "open_webui" {
+  jobspec = <<EOT
+job "openwebui" {
+  datacenters = ["dc1"]
+  type        = "service"
+  node_pool   = "gpu"
 
-#   group "openwebui" {
+  group "openwebui" {
 
-#     volume "openwebui" {
-#       type            = "host"
-#       source          = "${nomad_dynamic_host_volume.open_webui.name}"
-#       access_mode     = "single-node-writer"
-#       attachment_mode = "file-system"
-#     }
+    volume "openwebui" {
+      type            = "host"
+      source          = "${nomad_dynamic_host_volume.open_webui.name}"
+      access_mode     = "single-node-writer"
+      attachment_mode = "file-system"
+    }
 
-#     network {
-#       port "http" {
-#         to = 8080
-#       }
-#     }
+    network {
+      port "http" {
+        to = 8080
+      }
+    }
 
-#     service {
-#       name     = "openwebui"
-#       port     = "http"
-#       tags     = ["http"]
-#       provider = "nomad"
-#     }
+    service {
+      name     = "openwebui"
+      port     = "http"
+      tags     = ["http"]
+      provider = "nomad"
+    }
 
-#     task "openwebui" {
-#       driver = "docker"
+    task "openwebui" {
+      driver = "docker"
 
-#       config {
-#         image   = "ghcr.io/open-webui/open-webui:main"
-#         ports   = ["http"]
-#       }
+      config {
+        image   = "ghcr.io/open-webui/open-webui:main"
+        ports   = ["http"]
+      }
 
-#       resources {
-#         device "nvidia/gpu" {
-#           count = 1
-#         }
-#       }
+      resources {
+        device "nvidia/gpu" {
+          count = 1
+        }
+      }
 
-#       volume_mount {
-#         volume      = "openwebui"
-#         destination = "/app/backend/data"
-#       }
+      volume_mount {
+        volume      = "openwebui"
+        destination = "/app/backend/data"
+      }
 
-#       template {
-#         data = <<EOF
-# {{- range nomadService "ollama" }}OLLAMA_BASE_URL="http://{{ .Address }}:{{ .Port }}"{{ end -}}
-# EOF
-#         destination   = "/app/env.txt"
-#         env           = true
-#         change_mode   = "restart"
-#       }
-#     }
-#   }
-# }
-# EOT
-# }
+      template {
+        data = <<EOF
+{{- range nomadService "ollama" }}OLLAMA_BASE_URL="http://{{ .Address }}:{{ .Port }}"{{ end -}}
+EOF
+        destination   = "/app/env.txt"
+        env           = true
+        change_mode   = "restart"
+      }
+    }
+  }
+}
+EOT
+}
