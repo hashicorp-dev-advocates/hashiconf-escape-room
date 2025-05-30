9613d7f8aaa63a9dc7e4c50d5c4db0749033bf5e
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2025-05-30 15:05:56 -0400

    Remove node pools for now

--- nomad-node-pools/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "hcp_packer_artifacts" {
-  value       = { for a in data.hcp_packer_artifact.packer : a.bucket_name => a.external_identifier... }
-  description = "AMI IDs used with HCP Packer buckets for images"
-}
