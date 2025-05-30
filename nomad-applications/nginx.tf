41f82e2a0663d5d87598adfffa3f6a911c224837
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2025-05-30 13:25:43 -0400

    Deploy nginx as ingress for Open WebUI

---
+++ nomad-applications/nginx.tf
@@ -0,0 +1,61 @@
+resource "nomad_job" "nginx" {
+  jobspec = <<EOT
+job "nginx" {
+  datacenters = ["dc1"]
+
+  group "nginx" {
+    count = 1
+
+    network {
+      port "http" {
+        static = 8080
+      }
+    }
+
+    service {
+      name     = "nginx"
+      port     = "http"
+      provider = "nomad"
+    }
+
+    task "nginx" {
+      driver = "docker"
+
+      config {
+        image = "nginx"
+
+        ports = ["http"]
+
+        volumes = [
+          "local:/etc/nginx/conf.d",
+        ]
+      }
+
+      template {
+        data = <<EOF
+upstream backend {
+{{ range nomadService "openwebui" }}
+  server {{ .Address }}:{{ .Port }};
+{{ else }}server 127.0.0.1:65535; # force a 502
+{{ end }}
+}
+
+server {
+   listen 8080;
+
+   location / {
+      proxy_pass http://backend;
+   }
+}
+EOF
+
+        destination   = "local/load-balancer.conf"
+        change_mode   = "signal"
+        change_signal = "SIGHUP"
+      }
+    }
+  }
+}
+
+EOT
+}
