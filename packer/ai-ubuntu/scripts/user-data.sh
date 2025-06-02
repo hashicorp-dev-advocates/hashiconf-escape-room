71f9994d3806478bab687c6c8ab10690d20e0dc9
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2025-06-02 14:54:57 -0400

    Update AI image to include /var/lib/docker mount

--- packer/ai-ubuntu/scripts/user-data.sh
+++ packer/ai-ubuntu/scripts/user-data.sh
@@ -4,13 +4,21 @@ TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-meta
 NODE_POOL=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/tags/instance/NodePool)
 AWS_REGION=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/region)
 
-# Add EBS volume for Docker
+# Add EBS volume for Nomad host volumes
 mkfs -t xfs /dev/nvme1n1
 mkdir -p /etc/nomad.d/data/host_volumes
 mount /dev/nvme1n1 /etc/nomad.d/data/host_volumes
 
 chgrp nomad /etc/nomad.d/data/host_volumes
 chmod g+rwx /etc/nomad.d/data/host_volumes
+
+# Add EBS volume for Docker
+mkfs -t xfs /dev/nvme2n1
+mkdir -p /var/lib/docker
+mount /dev/nvme2n1 /var/lib/docker
+
+chgrp docker /var/lib/docker
+chmod g+rwx /var/lib/docker
 
 # Nomad configuration files
 cat <<EOF > /etc/nomad.d/nomad.hcl
