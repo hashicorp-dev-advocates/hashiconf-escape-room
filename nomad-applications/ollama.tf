8550faf1b94f48917f1d8c3290b726254d694b2e
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2025-06-02 16:18:08 -0400

    Remove applications again

--- nomad-applications/ollama.tf
+++ nomad-applications/ollama.tf
@@ -1,86 +1,86 @@
-resource "nomad_dynamic_host_volume" "ollama" {
-  name      = "ollama"
-  namespace = "default"
+# resource "nomad_dynamic_host_volume" "ollama" {
+#   name      = "ollama"
+#   namespace = "default"
 
-  plugin_id = "mkdir"
+#   plugin_id = "mkdir"
 
-  capacity_max = "12.0 GiB"
-  capacity_min = "8.0 GiB"
+#   capacity_max = "12.0 GiB"
+#   capacity_min = "8.0 GiB"
 
-  capability {
-    access_mode     = "single-node-writer"
-    attachment_mode = "file-system"
-  }
+#   capability {
+#     access_mode     = "single-node-writer"
+#     attachment_mode = "file-system"
+#   }
 
-  node_pool = "llm"
-}
+#   node_pool = "llm"
+# }
 
-resource "nomad_job" "ollama" {
-  jobspec = <<EOT
-job "ollama" {
-  datacenters = ["dc1"]
-  type        = "service"
-  node_pool   = "llm"
+# resource "nomad_job" "ollama" {
+#   jobspec = <<EOT
+# job "ollama" {
+#   datacenters = ["dc1"]
+#   type        = "service"
+#   node_pool   = "llm"
 
-  group "ollama" {
+#   group "ollama" {
 
-    volume "ollama" {
-      type            = "host"
-      source          = "${nomad_dynamic_host_volume.ollama.name}"
-      access_mode     = "single-node-writer"
-      attachment_mode = "file-system"
-    }
+#     volume "ollama" {
+#       type            = "host"
+#       source          = "${nomad_dynamic_host_volume.ollama.name}"
+#       access_mode     = "single-node-writer"
+#       attachment_mode = "file-system"
+#     }
 
-    network {
-      port "http" {
-        static = 11434
-      }
-    }
+#     network {
+#       port "http" {
+#         static = 11434
+#       }
+#     }
 
-    service {
-      name     = "ollama"
-      port     = "http"
-      tags     = ["http"]
-      provider = "nomad"
-    }
+#     service {
+#       name     = "ollama"
+#       port     = "http"
+#       tags     = ["http"]
+#       provider = "nomad"
+#     }
 
-    task "ollama" {
-      driver = "docker"
+#     task "ollama" {
+#       driver = "docker"
 
-      config {
-        image   = "ollama/ollama"
-        ports   = ["http"]
-      }
+#       config {
+#         image   = "ollama/ollama"
+#         ports   = ["http"]
+#       }
 
-      resources {
-        cores  = 2
-        memory = 32000
+#       resources {
+#         cores  = 2
+#         memory = 32000
 
-        device "nvidia/gpu" {
-          count = 1
-        }
-      }
+#         device "nvidia/gpu" {
+#           count = 1
+#         }
+#       }
 
-      volume_mount {
-        volume      = "ollama"
-        destination = "/root/.ollama"
-      }
+#       volume_mount {
+#         volume      = "ollama"
+#         destination = "/root/.ollama"
+#       }
 
-      env {
-        NVIDIA_VISIBLE_DEVICES     = "all"
-        NVIDIA_DRIVER_CAPABILITIES = "compute,utility"
-        CUDA_VISIBLE_DEVICES       = "0"
-      }
+#       env {
+#         NVIDIA_VISIBLE_DEVICES     = "all"
+#         NVIDIA_DRIVER_CAPABILITIES = "compute,utility"
+#         CUDA_VISIBLE_DEVICES       = "0"
+#       }
 
-      action "pull-model" {
-        command = "ollama"
-        args = [
-          "pull",
-          "${var.model}"
-        ]
-      }
-    }
-  }
-}
-EOT
-}
+#       action "pull-model" {
+#         command = "ollama"
+#         args = [
+#           "pull",
+#           "${var.model}"
+#         ]
+#       }
+#     }
+#   }
+# }
+# EOT
+# }
