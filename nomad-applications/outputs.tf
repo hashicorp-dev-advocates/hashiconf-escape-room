e3d68adbd80beaface7e80e11e3981d0109f07c1
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2025-06-03 11:31:13 -0400

    Output Open WebUI username and password

--- nomad-applications/outputs.tf
+++ nomad-applications/outputs.tf
@@ -1,3 +1,13 @@
 output "openweb_ui_url" {
   value = "http://${aws_lb.open_webui.dns_name}"
 }
+
+output "open_webui_username" {
+  value = "demos@hashicorp.com"
+}
+
+output "open_webui_password" {
+  value     = random_password.open_webui_token.result
+  sensitive = true
+}
+
