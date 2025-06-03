4abd17642c6a063dba30f206b986d74651396ef5
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2025-06-03 15:22:19 -0400

    Enable Websockets on nginx

--- nomad-applications/nginx.tf
+++ nomad-applications/nginx.tf
@@ -45,6 +45,9 @@ server {
 
    location / {
       proxy_pass http://backend;
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection "upgrade";
    }
 }
 EOF
