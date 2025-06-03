b7c3f2d4ec0eb8376ef4bd549aa07ad30e1dc820
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2025-06-03 11:34:16 -0400

    Regenerate Open WebUI password

--- nomad-applications/outputs.tf
+++ nomad-applications/outputs.tf
@@ -6,8 +6,18 @@ output "open_webui_username" {
   value = "demos@hashicorp.com"
 }
 
+resource "random_password" "open_webui_password" {
+  length           = 16
+  special          = true
+  override_special = "!*-"
+  min_lower        = 1
+  min_upper        = 1
+  min_numeric      = 1
+  min_special      = 1
+}
+
 output "open_webui_password" {
-  value     = random_password.open_webui_token.result
+  value     = random_password.open_webui_password.result
   sensitive = true
 }
 
