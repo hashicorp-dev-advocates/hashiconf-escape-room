5b2ffaf9869a569f6162d1741304e6ed344a27f0
Michael Kosir <michael.kosir@hashicorp.com>
2025-09-05 12:13:53 -0400

    Change ACL token expiration to hour format

--- nomad-applications/acl.tf
+++ nomad-applications/acl.tf
@@ -32,5 +32,5 @@ resource "nomad_acl_token" "contestants" {
   name     = nomad_acl_policy.contestants.name
   type     = "client"
   policies = [nomad_acl_policy.contestants.name]
-  expiration_ttl = "30d"
+  expiration_ttl = "720h"  # 30 days
 }
