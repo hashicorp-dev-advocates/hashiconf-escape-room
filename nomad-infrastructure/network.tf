ec87fb664c603d66b7f61255fa42d14e561aac3b
Michael Kosir <michael.kosir@hashicorp.com>
2025-10-01 12:17:11 -0500

    uncomment

--- nomad-infrastructure/network.tf
+++ nomad-infrastructure/network.tf
@@ -28,56 +28,56 @@ module "vpc" {
   }
 }
 
-# resource "aws_lb" "nomad" {
-#   name               = "nomad-lb"
-#   internal           = false
-#   load_balancer_type = "application"
-#   security_groups    = [aws_security_group.web.id, aws_security_group.nomad.id, aws_security_group.egress.id]
-#   subnets            = module.vpc.public_subnets
+resource "aws_lb" "nomad" {
+  name               = "nomad-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.web.id, aws_security_group.nomad.id, aws_security_group.egress.id]
+  subnets            = module.vpc.public_subnets
 
-#   enable_deletion_protection = false
+  enable_deletion_protection = false
 
-#   tags = {
-#     Name = "nomad-lb"
-#   }
-# }
+  tags = {
+    Name = "nomad-lb"
+  }
+}
 
-# resource "aws_lb_target_group" "nomad" {
-#   name        = "nomad-tg"
-#   port        = 4646
-#   protocol    = "HTTP"
-#   vpc_id      = module.vpc.vpc_id
-#   target_type = "instance"
+resource "aws_lb_target_group" "nomad" {
+  name        = "nomad-tg"
+  port        = 4646
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
+  target_type = "instance"
 
-#   health_check {
-#     enabled             = true
-#     path                = "/v1/agent/health"
-#     interval            = 10
-#     timeout             = 5
-#     healthy_threshold   = 2
-#     unhealthy_threshold = 10
-#     matcher             = "200"
-#   }
+  health_check {
+    enabled             = true
+    path                = "/v1/agent/health"
+    interval            = 10
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 10
+    matcher             = "200"
+  }
 
 
-# }
-# resource "aws_lb_target_group_attachment" "nomad" {
-#   count            = var.server_count
-#   target_group_arn = aws_lb_target_group.nomad.arn
-#   target_id        = aws_instance.nomad_servers[count.index].id
-#   port             = 4646
-# }
+}
+resource "aws_lb_target_group_attachment" "nomad" {
+  count            = var.server_count
+  target_group_arn = aws_lb_target_group.nomad.arn
+  target_id        = aws_instance.nomad_servers[count.index].id
+  port             = 4646
+}
 
-# resource "aws_lb_listener" "web" {
-#   load_balancer_arn = aws_lb.nomad.arn
-#   port              = 80
-#   protocol          = "HTTP"
+resource "aws_lb_listener" "web" {
+  load_balancer_arn = aws_lb.nomad.arn
+  port              = 80
+  protocol          = "HTTP"
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.nomad.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nomad.arn
+  }
+}
 
 data "hcp_hvn" "main" {
   hvn_id = data.terraform_remote_state.hcp.outputs.hvn.id
